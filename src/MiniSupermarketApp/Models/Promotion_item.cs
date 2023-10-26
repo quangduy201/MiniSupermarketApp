80efcb3631c41b53d5f331adf17252aaf79e341b
LongBOTT <93486974+LongBOTT@users.noreply.github.com>
2023-10-26 22:10:02 +0700

    Update Promotion_item.cs

--- src/MiniSupermarketApp/Models/Promotion_item.cs
+++ src/MiniSupermarketApp/Models/Promotion_item.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace MiniSupermarketApp.Models
 {
-    internal class Promotion_item
+    class Promotion_item
     {
+        int promotion_id;
+        int product_id;
+        double quantity;
+
+        public Promotion_item()
+        {
+        }
+
+        public Promotion_item(int promotion_id, int product_id, double quantity)
+        {
+            this.promotion_id = promotion_id;
+            this.product_id = product_id;
+            this.quantity = quantity;
+        }
+
+        public int Promotion_id
+        {
+            get { return promotion_id; }
+            set { promotion_id = value; }
+        }
+
+        public int Product_id
+        {
+            get { return product_id; }
+            set { product_id = value; }
+        }
+
+        public double Quantity
+        {
+            get { return quantity; }
+            set { quantity = value; }
+        }
+
+        public override string ToString()
+        {
+            return promotion_id + " | " +
+                product_id + " | " +
+                quantity;
+        }
     }
 }
